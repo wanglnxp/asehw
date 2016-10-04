f71ca89c72fb34fb616d753a39d4c9e0eac8b8de
Linnan Wang <lw2645@columbia.edu>
2016-10-04 01:41:27 -0400

    pass static analysis

--- 01_basicApp/main.py
+++ 01_basicApp/main.py
@@ -1,67 +1,73 @@
+"""
+This is Toy System for homework 2
+The website can store user message and retrive all of them
+"""
+
 import os
-from sqlalchemy import *
+# import traceback
+from sqlalchemy import create_engine
 from flask import Flask, request, render_template, g, redirect
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
-date_of_game = []
-
-app = Flask(__name__)
+TMPL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+APP = Flask(__name__, template_folder=TMPL_DIR)
 
 DATABASEURI = "postgresql://Linnan@localhost:5432/mydb"
-
-engine = create_engine(DATABASEURI)
-
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+ENGINE = create_engine(DATABASEURI)
 
 
-@app.before_request
+@APP.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
+    """
+    This function is run at the beginning of every web request
+    (every time you enter an address in the web browser).
+    We use it to setup a database connection that can be used throughout the request.
 
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    import traceback; traceback.print_exc()
-    g.conn = None
+    The variable g is globally accessible.
+    """
+    try:
+        g.conn = ENGINE.connect()
+    except Exception:
+        print "uh oh, problem connecting to database"
+        # traceback.print_exc()
+        g.conn = None
 
 
-@app.teardown_request
+@APP.teardown_request
 def teardown_request(exception):
     """
     At the end of the web request, this makes sure to close the database connection.
     If you don't, the database could run out of memory!
     """
     try:
-      g.conn.close()
-    except Exception as e:
-      pass
+        g.conn.close()
+    except Exception:
+        print exception
 
 
-@app.route('/')
+@APP.route('/')
 def index():
-    print request.args
+    """
+    access the main page
+    :return:
+    """
     return render_template("index.html", **locals())
 
 
-@app.route('/error')
+@APP.route('/error')
 def error():
-    print request.args
+    """
+    access the error page
+    :return:
+    """
     return render_template("error.html", **locals())
 
 
-@app.route('/add', methods=['POST'])
+@APP.route('/add', methods=['POST'])
 def add():
+    """
+    add user enter message and user name to database
+    :return: main page
+    """
     name = request.form['name']
     message = request.form['message']
 
@@ -69,27 +75,29 @@ def add():
         newcurs = g.conn.execute("""INSERT INTO record
         VALUES (%s, %s );""", name, message)
         newcurs.close()
-    except:
+    except Exception:
+        print "can not write record to database"
         return redirect('/error')
 
     return render_template("index.html", **locals())
 
 
-@app.route('/record')
+@APP.route('/record')
 def record():
-    print request.args
+    """
+    access record page where can see all the data in database
+    :return: record page
+    """
 
     try:
         newcurs = g.conn.execute("""SELECT * FROM record""")
-    except:
-        print "database error"
-        return "<h1>It is database error<h1>"
+    except Exception:
+        print "can not read record from database"
+        return redirect('/error')
 
     info = []
     for result in newcurs:
-        temp = []
-        temp.append(result['user_name'].encode("utf-8"))
-        temp.append(result['message'].encode("utf-8"))
+        temp = [result['user_name'].encode("utf-8"), result['message'].encode("utf-8")]
         info.append(temp)
     newcurs.close()
 
@@ -99,29 +107,6 @@ def record():
     return render_template("record.html", **locals())
 
 
-@app.route('/bacon', methods=['GET', 'POST'])
-def bacon():
-    if request.method == 'POST':
-        return "You're using POST"
-    else:
-        return "You're using using GET"
-
-
-@app.route('/tuna')
-def tuna():
-    return '<h1>Tuna is good</h1>'
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    return '<h2>Welcome %s</h2>' % username
-
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    post_id *= 5
-    return '<h2>This is your post ID * 5 = %s</h2>" ' % post_id
-
-
-if __name__ == "__main__":  # check whether the file is called directly, otherwise do not run
-    app.run(debug=true)
+if __name__ == "__main__":
+    # check whether the file is called directly, otherwise do not run
+    APP.run(debug=True)
